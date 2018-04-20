29357fe71a5b33a389a36d444b024a5669a82e06
david5_huang <david.huang@askeycloud.com>
2018-04-20 14:59:07 +0800

    Initial Commit

---
+++ AWSIoTCertTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWSIoTCertTest
+//
+//  Created by david5_huang on 2018/4/20.
+//  Copyright © 2018年 david5_huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
